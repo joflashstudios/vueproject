73bba245816529c42211ebda1784cd004d9f9861
joflashstudios <joflashstudios@gmail.com>
2017-07-10 22:13:28 -0500

    cleanup

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
-const exampleDependencyInjector = require("!!vue-loader?inject!@/components/Hello")
-const testComponent = exampleDependencyInjector({
+const helloInjector = require("!!vue-loader?inject!@/components/Hello")
+
+const Hello = helloInjector({
   "../modules/dependency": {
     getTwelve() {
       return  13;
@@ -11,7 +11,7 @@ const testComponent = exampleDependencyInjector({
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(testComponent)
+    const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App13')
